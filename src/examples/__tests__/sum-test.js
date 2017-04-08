e2c7298bd4a6b0c2ad4c76ea8715a98831c1ccf3
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-04-09 03:04:13 +0700

    Add files

---
+++ src/examples/__tests__/sum-test.js
@@ -0,0 +1,9 @@
+// const sum = require('../sum');
+import sum from '../sum';
+
+describe('Sum', () => {
+  it('should return correct answer', () => {
+    let result = sum(2, 2);
+    expect(result).toBe(4);
+  });
+});
